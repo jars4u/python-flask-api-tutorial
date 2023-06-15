31e27f312df833984b49658302e9963dab229cac
Juan Ramos <127767142+jars4u@users.noreply.github.com>
2023-06-15 21:44:20 +0000

    first endpoint

---
+++ src/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return "<h1>Hello!</h1>"
+
+
+
+
+
+
+
+
+
+
+
+# ESTAS LINEAS SIEMPRE AL FINAL:
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
