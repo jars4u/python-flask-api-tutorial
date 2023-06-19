300eca5c5be78fd696a7ff5a87e170392c2dc19b
Juan Ramos <127767142+jars4u@users.noreply.github.com>
2023-06-19 16:35:44 +0000

    project done

--- src/app.py
+++ src/app.py
@@ -1,25 +1,39 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-todos = [{ "label": "Mi primera tarea", "done": False }]
+todos = [{"label": "Mi primera tarea", "done": False}]
+
+
+@app.route('/health_check', methods=['GET'])
+def health_check():
+    return "OK"
+
 
 @app.route('/todos', methods=['GET'])
 def get_todos():
     json_todos = jsonify(todos)
     return json_todos
 
+
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.json
-    print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
+    todos.append(request_body)
+    return todos
+
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    for item in range(len(todos)):
+        if item == position:
+
+            todos.remove(todos[item])
+    print("This is the position to delete: ",position)
+    return todos
 
 
 
-
-
-
-
+                
 
 
 
